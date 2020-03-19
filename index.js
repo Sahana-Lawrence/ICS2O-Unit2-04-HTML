b60a004318847c1e216120de8c9b3735a428292e
Sahana-Lawrence <sahana.lawrence@mths.ca>
2020-03-19 16:13:55 +0000

    updated files

---
+++ index.js
@@ -0,0 +1,17 @@
+// JavaScript File
+
+// change background color to lightblue:
+function changeBodyBg(color){
+  document.body.style.backgroundColor = "lightblue";
+}
+
+// addEventListener
+document.addEventListener("click", function(alert){
+  document.getElementById("button");
+  alert ("Hello!")
+});
+
+// change value of text field
+function myFunction(text) {
+  document.getElementById("text").value = "Shawn";
+}
